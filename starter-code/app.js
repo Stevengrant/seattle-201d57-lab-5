eb119327e25ae28fb4877caa8c872265c34ca2d6
Steven Grant <blue.ninjaneer@gmail.com>
2019-06-07 11:47:57 -0700

    added multiplication

--- starter-code/app.js
+++ starter-code/app.js
@@ -32,10 +32,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+  let productRes = a * b,
+    resMessage = `The product of ${a} and ${b} is ${productRes}.`,
+    res = [productRes, resMessage];
+  return res;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
